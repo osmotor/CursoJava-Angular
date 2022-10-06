b1c6d177c2c55b780760b9d85d1d91d35b561173
osmotor <115150873+osmotor@users.noreply.github.com>
2022-10-06 21:53:28 +0200

    desde casa

--- Proy003_Bank/src/main/java/com/curso/bank/Bank.java
+++ Proy003_Bank/src/main/java/com/curso/bank/Bank.java
@@ -4,22 +4,22 @@ public class Bank {
 
 public static void main(String[] args) {
 		
-		Account cuenta1 = new Account(0);
-		Customer c1 = new Customer("Oscar", "Torres");
-		
-		c1.setAccount(cuenta1);
-		c1.getAccount().deposit(500);
-		System.out.printf("\rCreando el cliente "+ 
-				c1.getFirstName() + " " + c1.getLastName());
-		System.out.printf("\rCreando su cuenta bancario con saldo "+ c1.getAccount().getBalance() + ".");
+	Account cuenta1 = new Account(0);
+	Customer c1 = new Customer("Rafa", "Antúnez");
+	
+	c1.setAccount(cuenta1);
+	c1.getAccount().deposit(500);
+	System.out.printf("\rCreando el cliente "+ 
+			c1.getFirstName() + " " + c1.getLastName());
+	System.out.printf("\rCreando su cuenta bancario con saldo "+ c1.getAccount().getBalance() + ".");
 
-		System.out.printf("\rSacar 200: "+ c1.getAccount().withdraw(200));
-		
-		System.out.printf("\rIngresar 50: "+ c1.getAccount().deposit(50));
-		System.out.printf("\rSacar 60: "+ c1.getAccount().withdraw(60));
-		System.out.printf("\rSacar 700: "+ c1.getAccount().withdraw(700));
+	System.out.printf("\rSacar 150.00: "+ c1.getAccount().withdraw(150));
+	
+	System.out.printf("\rIngresar 22.50: "+ c1.getAccount().deposit(22.50));
+	System.out.printf("\rSacar 47.62: "+ c1.getAccount().withdraw(47.62));
+	System.out.printf("\rSacar 400: "+ c1.getAccount().withdraw(400));
 
-		System.out.printf("\rCustomer: ["+  c1.getFirstName() + ", "+ c1.getLastName() + "] tiene un saldo de: " + c1.getAccount().getBalance());
-		System.out.println(" ");
+	System.out.printf("\rCustomer: ["+  c1.getFirstName() + ", "+ c1.getLastName() + "] tiene un saldo de: " + c1.getAccount().getBalance());
+	System.out.println(" ");
 	}
 }
